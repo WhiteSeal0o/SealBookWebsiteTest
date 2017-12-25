61919e7843f2b2d33f035977db8e98f87f9c48c8
Hoang <20155637@student.hust.edu.vn>
2017-12-26 05:56:04 +0700

    get host info to deploy

--- define_variable.php
+++ define_variable.php
@@ -1,8 +1,30 @@
 <?phP
-    define('HOST', 'localhost');
-    define('USERNAME', 'root');
-    define('PASSWORD', '');
+
     define('DBNAME', 'qlsach');
 
     define('RECORDS_PER_PAGE', '12');
+
+    $connectstr_dbhost = '';
+    $connectstr_dbname = '';
+    $connectstr_dbusername = '';
+    $connectstr_dbpassword = '';
+
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
+        }
+        
+        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
+
+    define('NAME', $connectstr_dbname);
+
+    define('USERNAME', $connectstr_dbusername);
+
+    define('PASSWORD', $connectstr_dbpassword);
+
+    define('HOST', $connectstr_dbhost);
 ?>
